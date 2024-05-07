3d27f506d752dc443c635b87cc176ddad2ea1e91
stiven <stivenchoo@gmail.com>
2024-05-06 23:10:48 -0500

    fix theproductlist

--- resources/js/components/products/TheProductList.vue
+++ resources/js/components/products/TheProductList.vue
@@ -13,7 +13,7 @@
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="icon icon-tabler icons-tabler-outline icon-tabler-prouductmark"
+            class="icon icon-tabler icons-tabler-outline icon-tabler-productmark"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M18 7v14l-6 -4l-6 4v-14a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4z" />
@@ -24,7 +24,7 @@
       </div>
       <div class="card-body bg-table" >
         <div class="table-responsive my-4 mx-2">
-          <table class="col-12 bg-table" id="prouduct_table">
+          <table class="col-12 bg-table" id="product_table">
             <thead style="height: 3em">
               <tr>
                 <th>Titulo</th>
@@ -36,21 +36,21 @@
             </thead>
             <tbody>
               <tr
-                v-for="(prouduct, index) in products"
+                v-for="(product, index) in products"
                 :key="index"
                 class="bg-custom-row"
                 style="height: 3em"
               >
-                <td>{{ prouduct.title }}</td>
-                <td>{{ prouduct.author.name }}</td>
-                <td>{{ prouduct.category.name }}</td>
-                <td>{{ prouduct.stock }}</td>
+                <td>{{ product.title }}</td>
+                <td>{{ product.author.name }}</td>
+                <td>{{ product.category.name }}</td>
+                <td>{{ product.stock }}</td>
                 <td>
                   <div class="d-flex justify-content-center" title="Editar">
                     <button
                       type="button"
                       class="btn btn-warning btn-sm"
-                      @click="editprouduct(prouduct)"
+                      @click="editproduct(product)"
                     >
                       <i class="fas fa-pencil-alt"></i>
                     </button>
@@ -58,7 +58,7 @@
                       type="button"
                       class="btn btn-danger btn-sm ms-2"
                       title="Eliminar"
-                      @click="deletprouduct(prouduct)"
+                      @click="deletproduct(product)"
                     >
                       <i class="fas fa-trash-alt"></i>
                     </button>
@@ -70,10 +70,10 @@
         </div>
       </div>
       <div>
-        <prouduct-modal
+        <product-modal
           :authors_data="authors_data"
-          :prouduct_data="prouduct"
-          ref="prouduct_modal"
+          :product_data="product"
+          ref="product_modal"
         />
       </div>
     </div>
@@ -81,18 +81,18 @@
 </template>
 
 <script>
-import prouductModal from "./ProductModal.vue";
+import productModal from "./ProductModal.vue";
 import { deleteMessage, successMessage } from "../../helpers/Alerts";
 
 export default {
   components: {
-    prouductModal,
+    productModal,
   },
   props: ["products", "authors_data"],
   data() {
     return {
       modal: null,
-      prouduct: {},
+      product: {},
     };
   },
   mounted() {
@@ -100,18 +100,18 @@ export default {
   },
   methods: {
     async index() {
-      $("#prouduct_table").DataTable();
-      const modal_id = document.getElementById("prouduct_modal");
+      $("#product_table").DataTable();
+      const modal_id = document.getElementById("product_modal");
       this.modal = new bootstrap.Modal(modal_id);
       modal_id.addEventListener("hidden.bs.modal", (e) => {
-        this.$refs.prouduct_modal.reset();
+        this.$refs.product_modal.reset();
       });
     },
-    editprouduct(prouduct) {
-      this.prouduct = prouduct;
+    editproduct(product) {
+      this.product = product;
       this.openModal();
     },
-    async deletprouduct({ id }) {
+    async deletproduct({ id }) {
       if (!(await deleteMessage())) return;
       try {
         await axios.delete(`/products/${id}`);
